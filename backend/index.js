e60a0421f8b61d848483d361c4bacf4627aec5af
Seloaziz <seloaziz21@gmail.com>
2026-01-03 12:13:21 +0700

    add my domain to BE

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(
   cors({
-    origin: 'http://localhost:3001',
+    origin: [
+      'http://localhost:3001',
+      'https://sirjobsir.ac.id',
+      'https://www.sirjobsir.ac.id',
+    ],
     credentials: true,
   })
 )
