596884fbbe43479d52783554a641f9ab886f02e6
Seloaziz <seloaziz21@gmail.com>
2026-01-05 01:03:56 +0700

    delete debug (2)

--- backend/index.js
+++ backend/index.js
@@ -28,6 +28,7 @@ const corsOptions = {
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }
+app.options('*', cors())
 
 app.use(cors(corsOptions))
 
