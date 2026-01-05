af7983b09fb063cae4cb91c9e1eef0d64a60bb88
Seloaziz <seloaziz21@gmail.com>
2026-01-05 10:10:15 +0700

    add csl log

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ const app = express()
 
 app.use((req, res, next) => {
   if (req.method === 'OPTIONS') {
+    console.log('bububububu')
     res.header('Access-Control-Allow-Origin', req.headers.origin)
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
